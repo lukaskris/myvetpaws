2fd0088a218730f4fdbafd43a26428175aa40e41
lukaskris12 <lukaskris12@gmail.com>
2025-07-03 09:46:53 +0700

    update db customer add delete field

--- database/migrations/2025_06_12_132353_create_customers_table.php
+++ database/migrations/2025_06_12_132353_create_customers_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
