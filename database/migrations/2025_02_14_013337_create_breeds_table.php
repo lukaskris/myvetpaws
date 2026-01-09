738a38a5fd41f5bf3d185ad7fd82c5185685ae2b
Goe.Ricky Christian <rickychristian2309@gmail.com>
2026-01-09 11:15:42 +0700

    fix: bug in breeds

--- database/migrations/2025_02_14_013337_create_breeds_table.php
+++ database/migrations/2025_02_14_013337_create_breeds_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('breeds')) {
+            return;
+        }
+
         Schema::create('breeds', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
